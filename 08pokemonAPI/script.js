91bcd260c9e825629af209f5021ec8739b16facf
leonardoypla <yplacordeiro@gmail.com>
2023-03-25 13:30:56 -0300

    pokeapi edits

--- 08pokemonAPI/script.js
+++ 08pokemonAPI/script.js
@@ -1,39 +1,39 @@
-// 0 - Adicionar evento em botão para executar contexto da função
-async function executar(){
-    console.log("Teste")
-    // 1 - Pegar o valor digitado input (id do personagem)
-    const elementoInput = document.getElementById("idPokemon")
-    const id = elementoInput.value // ! Substituir pelo valor do INPUT !
-    // 2 - Buscar personagem da API (já está pronto)
-    const pokemon = await buscarPokemonDaAPI(id)
 
-    /* MONTAR ABAIXO HTML COM O VALOR RETORNADO DA API */
-    // 3 - Pegar elemento de resultado do HTML
-    const elementoImg = document.getElementById("imgPokemon")
-    const elementoNome = document.getElementById("nomePokemon")
-    // const elementoCor = document.getElementById("corPokemon")
+window.addEventListener("load", function() {
+    console.log("Sucesso!")
+    document.getElementById("botaoBusca").addEventListener("click", funcaoPrincipal)
+})
 
-    // 4 - Atribuir valores do personagem na tela
-    // 4.1 - Mudar o atributo src da tag img
-    elementoImg.src = pokemon.sprites.front_default
-    // 4.2 - Mudar o atributo innerText da tag que guarda o nome do personagem
-    elementoNome.innerHTML = "#" + id + " - " + pokemon.name
+funcaoPrincipal()
 
-    /* Dica:
-        Dar console.log na variável 'personagem'
-        para visualizar o objeto;
-        Olhar a documentação de como o objeto é retornado
-        https://rickandmortyapi.com/documentation/#character-schema
-    */
+async function funcaoPrincipal(){
+    // entrada
+    const idPokemon = pegarValue()
+    // processamento
+    const pokemon = await buscarPokemon(idPokemon)
+    // saida
+    mostrarDados(pokemon)
 }
 
+function pegarValue(){
+    const elementoInput = document.getElementById("idPokemon")
+    const valor = elementoInput.value
 
-/* Função para fazer uma request GET para a API utilizando o JS 
-   Veremos logo em seguida! Foquem na leitura do Objeto na função acima.
-*/
-async function buscarPokemonDaAPI(id){
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    return valor
+}
 
-    const response = await fetch(url)
-    return response.json()
+async function buscarPokemon(id){
+    const url = (`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const resposta = await fetch(url)
+    const pokemon = await resposta.json()
+
+    return pokemon
+}
+
+function mostrarDados(personagem){
+    const containerImgPokemon = document.getElementById("containerImgPokemon")
+    containerImgPokemon.innerHTML = `<img class="imgPokemon" src="${personagem.sprites.front_default}">`
+
+    const containerNomePokemon = document.getElementById("containerNomePokemon")
+    containerNomePokemon.innerHTML = `<p class="nomePokemon"> ${personagem.name} </p>`
 }
